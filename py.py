7ac106ba70665a55306984a21762631afc749eb3
ereghost <133422152+ereghost@users.noreply.github.com>
2023-05-15 15:41:20 +0300

    sensor datanın ortalamasını alan fonskiyon yazıldı py.py

--- py.py
+++ py.py
@@ -1,3 +1,11 @@
 collected = []
 sensor_data = []
 
+
+def sensor_data_to_average():
+    total = sum(sensor_data)
+    average = total / len(sensor_data)
+    if average <= 10:
+        return "Stop"
+    else:
+        return "Continue"
