dd31947347ee1d6335adf5983d4f17ed29ab7ace
ereghost <133422152+ereghost@users.noreply.github.com>
2023-05-15 15:43:08 +0300

    Kodun çalışmasını sağlayan geri kalan kodlar yazıldı py.py

--- py.py
+++ py.py
@@ -18,3 +18,12 @@ def collect_sensor_data(data_point):
     sensor_data.append(data_point)
     sensor_data_check()
     collected.append(sensor_data_to_average())
+    
+if __name__ == "__main__":
+    sensor_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    data_points = [12, 23, 34, 56, 73, 23, 45, 67, 23, 45, 12, 34, 12, 34, 56, 34]
+    
+    for data_point in data_points:
+        collect_sensor_data(data_point)
+
+    print(collected[-1])
